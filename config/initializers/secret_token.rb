f9611e08bce413d86470a4cd047a1ea0f5b8752b
bstrech <bstrech@enphaseenergy.com>
2014-01-28 23:07:52 -0800

    first app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tourbooker::Application.config.secret_token = '4b51c064d023d34336cb52bba9c9f30f3b59945cec55a999d7f16a50967c9694011e653688bdd118f7ffb4c804e89395997166ef1055adf22a254537821a67be'
