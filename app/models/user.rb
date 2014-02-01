e2f58b16a655f0970a15405e7f2a71a9cb4c06ca
bstrech <bstrech@enphaseenergy.com>
2014-01-31 19:31:18 -0800

    More clean up of extra files and updated new to have the right fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   include AASM
   strip_attributes
-  attr_accessible :amn_doctor, :amn_movie_theater, :amn_pool, :amn_rec_room, :amn_time_machine, :email, :first_name, :ip_address, :last_name, :phone, :preferred_tour_date, :rating, :token
+  attr_accessible :amn_doctor, :amn_movie_theater, :amn_pool, :amn_rec_room, :amn_time_machine, :email, :first_name, :last_name, :phone, :preferred_tour_date, :rating, :token
   before_validation :downcase_email
   after_initialize :generate_token
   validates :email, presence: true, uniqueness: true, format: $EMAIL_FORMAT
